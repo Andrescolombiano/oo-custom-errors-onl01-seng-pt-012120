f94b6c599c584ea98b96d868f7e00f864573df51
Andres Carvajal <jc1993box@hotmail.com>
2020-03-19 02:56:41 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,6 +7,7 @@ class Person
  
   def get_married(person)
     self.partner = person
+<<<<<<< HEAD
     if person.class != Person
       begin
         raise PartnerError
@@ -25,6 +26,23 @@ class Person
   end
 end
  
+=======
+    if person.class != person
+      begin
+      raise PartnerError
+      rescue PartnerError => error 
+       puts error.message
+     else
+    person.partner = self
+  end
+end
+ 
+class PartnerError < StandError
+  def message
+    "you must give the get_married method an argument of an instance of the person class!"
+end
+ 
+>>>>>>> d87dee812443594e4ef3acfe934a14346b8b60af
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
